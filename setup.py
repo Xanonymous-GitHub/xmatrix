507af2e823a409b54c872e94fae3938e103e6d73
Xanonymous <trusaidlin@gmail.com>
2020-03-09 02:02:43 +0800

    package all

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="example-pkg-YOUR-USERNAME-HERE",
+    version="1.0",
+    author="Xanonymous",
+    author_email="trusaidlin@gmail.com",
+    description="Help you calculate matrix.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Xanonymous-GitHub/xmatrix",
+    packages=find_packages(),
+    platforms=["all"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
