f287a7c96a8f2452c2732967ba4f261e3149d66e
Xanonymous <trusaidlin@gmail.com>
2020-03-09 02:13:50 +0800

    release 1.02

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="xmatrix",
-    version="1.0",
+    version="1.02",
     author="Xanonymous",
     author_email="trusaidlin@gmail.com",
     description="Help you calculate matrix.",
